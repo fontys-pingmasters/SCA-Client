a57e463dbdae35effd4484e40bfe75dbd037e46a
Erdem <513369@student.fontys.nl>
2024-10-15 13:56:43 +0200

    Begin component

--- sca_frontend/sca_frontend/src/pages/SignInPage.tsx
+++ sca_frontend/sca_frontend/src/pages/SignInPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import mediaanConclusionLogo from '/mediaan_conclusion_logo.png'
 
 function SignInPage() {
     return (
-      <div>
-        fbhdfgb
+      <div className="whitebox">
+        <img src={mediaanConclusionLogo} className="logo" alt="Mediaan Conclusion Logo" />
+
       </div>
     );
   }
