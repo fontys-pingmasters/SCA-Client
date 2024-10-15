c8939b9585f88db1a7f03f5dc9a05da7e1e73cde
Erdem <513369@student.fontys.nl>
2024-10-15 10:13:44 +0200

    Auth0 verwijderen

--- sca_frontend/src/index.js
+++ sca_frontend/src/index.js
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Context, Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-    domain="sjorsklaassen.eu.auth0.com"
-    clientId="rj08W8BK46dPu7RT5s3eZy6Gr6yJdj3u"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>
     <App />
   </React.StrictMode>
 );
