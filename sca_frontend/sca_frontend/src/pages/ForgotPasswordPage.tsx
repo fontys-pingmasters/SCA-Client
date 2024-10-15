41abf02b31218c2c25ff62f51f8680edddff1d30
Erdem <513369@student.fontys.nl>
2024-10-15 13:40:36 +0200

    Initial Branch Commit

---
+++ sca_frontend/sca_frontend/src/pages/ForgotPasswordPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+function ForgotPasswordPage() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+  
+  export default ForgotPasswordPage;
---
+++ sca_frontend/sca_frontend/src/pages/SignInPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+function SignInPage() {
+    return (
+      <div>
+        fbhdfgb
+      </div>
+    );
+  }
+  
+  export default SignInPage;
