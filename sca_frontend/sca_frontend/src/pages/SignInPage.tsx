510333c3d34398831c6f2095d9418680d3c52c58
Allertvdw <allertvdw@hotmail.com>
2024-10-29 13:45:00 +0100

    Added login api call

--- sca_frontend/sca_frontend/src/pages/SignInPage.tsx
+++ sca_frontend/sca_frontend/src/pages/SignInPage.tsx
@@ -1,8 +1,44 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom'
 import mediaanConclusionLogo from '/mediaan_conclusion_logo.png'
 
 function SignInPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await loginRequest();
+    };
+
+    const loginRequest = async () => {
+        try {
+            const response = await fetch("https://localhost:7035/Auth/login" , {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            if (response.status === 401) {
+                return;
+            }
+
+            const data = await response.json();
+            localStorage.setItem("token", data);
+            console.log(data);
+            navigate("/homepage")
+        } catch (error) {
+            console.error("Error during login: ", error)
+            navigate("/login");
+        }
+    }
+
     return (
         <div className="flex items-center justify-center min-h-screen">
             <div className="bg-white p-8 rounded-lg shadow-lg w-80">
@@ -15,13 +51,17 @@ function SignInPage() {
                 <h2 className="text-2xl font-semibold text-black mb-2">Sign in</h2>
                 <p className="text-sm text-gray-500 mb-4">Sign in with your account.</p>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
-                        type="text"
-                        placeholder="Username"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        placeholder="Email"
                         className="w-full p-2 mb-3 border bg-white text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
                     />
                     <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         type="password"
                         placeholder="Password"
                         className="w-full p-2 mb-3 border bg-white text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
