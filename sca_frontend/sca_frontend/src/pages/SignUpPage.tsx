a94e15c746da2d690fe3a855e7c032ca37d9f0cb
Allertvdw <allertvdw@hotmail.com>
2024-11-05 13:05:57 +0100

    Added api call for register

--- sca_frontend/sca_frontend/src/pages/SignUpPage.tsx
+++ sca_frontend/sca_frontend/src/pages/SignUpPage.tsx
@@ -3,6 +3,44 @@ import { Link, useNavigate } from 'react-router-dom'
 import mediaanConclusionLogo from '/mediaan_conclusion_logo.png'
 
 function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await registerRequest();
+  }
+
+  const registerRequest = async () => {
+    try {
+      const response = await fetch("https://localhost:7035/Auth/register", {
+          method: "POST",
+          headers: {
+              "content-type": "application/json",
+          },
+          body: JSON.stringify({
+              email: email,
+              password: password,
+              confirmPassword: confirmPassword
+          }),
+      });
+
+      if (response.status === 401) {
+          return;
+      }
+
+      const data = await response.json();
+      console.log(data);
+      navigate("/")
+  } catch (error) {
+      console.error("Error during login: ", error)
+      navigate("/sign-up");
+  }
+}
+
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div>
@@ -12,28 +50,32 @@ function SignUpPage() {
           </div>
           <h2 className="text-2xl font-semibold text-black mt-2">Sign up</h2>
           <p className="text-sm text-gray-500 mb-4">Create your account.</p>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               placeholder="Email"
               className="w-full p-2 mb-3 border bg-white text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500" />
             <div className="mt-3">
               <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
                 placeholder="Password"
                 className="w-full p-2 mb-3 border bg-white text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500" />
               <input
-                type="confirmpassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
                 placeholder="Confirm password"
                 className="w-full p-2 mb-3 border bg-white text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500" />
             </div>
-            <Link to="/">
               <button
                 type="submit"
                 className="w-full p-2 rounded-md bg-gradient-to-r from-red-500 to-orange-500 text-white font-semibold">
                 Continue
               </button>
-            </Link>
           </form>
         </div>
         <div className="text-center mt-6">
