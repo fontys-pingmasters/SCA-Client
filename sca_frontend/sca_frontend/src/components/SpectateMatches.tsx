c605986d6c032122bc94730c89797849d18bf5e8
Erdem <513369@student.fontys.nl>
2024-11-12 15:10:01 +0100

    Update SpectateMatches.tsx

--- sca_frontend/sca_frontend/src/components/SpectateMatches.tsx
+++ sca_frontend/sca_frontend/src/components/SpectateMatches.tsx
@@ -7,7 +7,7 @@ interface SpectateMatchesProps {
 const SpectateMatches: React.FC<SpectateMatchesProps> = ({ className }) => (
   <div className={`flex flex-col ${className}`}>
     <h3 className="text-lg font-bold mb-2 text-black">Spectate Matches</h3>
-    <button className="drop-shadow-lg bg-gradient-to-r from-red-500 to-orange-500 text-white p-2 rounded-lg w-full  items-center">
+    <button className="drop-shadow-lg bg-gradient-to-r from-red-500 to-orange-500 text-white p-2 rounded-lg w-full items-center">
       View Live Matches
     </button>
   </div>
